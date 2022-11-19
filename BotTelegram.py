74b6002a4d4e90cba0d8634e2874bccb02a725aa
lucas <lucas.linard@outlook.com>
2022-11-19 17:28:24 -0300

    salvando as var com sucesso

--- BotTelegram.py
+++ BotTelegram.py
@@ -8,7 +8,19 @@ from random import randrange
 CHAVE_API = "5787455269:AAHeom_PmlWUL5jgeiiLkxOPN1Y9ycCjQfc"
 
 bot = telebot.TeleBot(CHAVE_API)
-user_text = ""
+
+user_dict = {}
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.id = None
+        self.medico = None
+        self.data = None
+        self.hora = None
+
+
 # @bot.message_handler(commands=["pizza"])
 # def pizza(mensagem):
 #     bot.send_message(mensagem.chat.id, "Saindo a pizza pra sua casa: Tempo de espera em 20min")
@@ -29,16 +41,19 @@ def write_json(data, filename='bd.json'):
 
 @bot.message_handler(commands=["MarcarConsulta"])
 def opcao1(mensagem):
+    chat_id = mensagem.chat.id
+    name = mensagem.from_user.first_name
+    user = User(name)
+    user_dict[chat_id] = user
     texto = """
     Qual é a sua necessidade? (Clique em uma opção)
     /ClinicoGeral Clinico Geral
     """
-    bot.send_message(mensagem.chat.id, texto)    
-    user_text = mensagem.text
+    msg = bot.reply_to(mensagem, texto)        
     #write_json(mensagem)
-    bot.register_next_step_handler(mensagem, EscolherClinicoGeral)
+    bot.register_next_step_handler(msg, EscolherClinicoGeral)
     
-def EscolherClinicoGeral(mensagem):    
+def EscolherClinicoGeral(mensagem):        
     with open('bd.json') as json_file:
         data = json.load(json_file)
         data = data["medicos"]
@@ -47,16 +62,16 @@ def EscolherClinicoGeral(mensagem):
         /1 - {data[0]["name"]}
         /2 - {data[1]["name"]}
         /3 - {data[2]["name"]}
-        """        
+        """                
         # markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
         # markup.add(data[0]["name"], data[1]["name"], data[2]["name"])
         # msg = bot.reply_to(mensagem, 'What is your gender', reply_markup=markup)        
-        bot.send_message(mensagem.chat.id, texto)
+        bot.reply_to(mensagem, texto)
         bot.register_next_step_handler(mensagem, EscolherDiaClinicoGeral)
 
 def EscolherDiaClinicoGeral(mensagem):        
-    dia =  date.today() 
-    texto = f"""
+    dia =  date.today()       
+    texto = f"""    
     Qual dos dias disponiveis você deseja marcar a consulta?
     /1 - {(dia).strftime("%d/%m/%Y")}
     /2 - {(dia + timedelta(days=randrange(30))).strftime("%d/%m/%Y")}
@@ -64,6 +79,11 @@ def EscolherDiaClinicoGeral(mensagem):
     /5 - {(dia + timedelta(days=randrange(50))).strftime("%d/%m/%Y")}
     /6 - {(dia + timedelta(days=randrange(70))).strftime("%d/%m/%Y")}
     """
+    chat_id = mensagem.chat.id
+    medico = mensagem.text
+    user = user_dict[chat_id]
+    user.medico = medico
+    
     bot.send_message(mensagem.chat.id, texto)
     bot.register_next_step_handler(mensagem, EscolherHorarioClinicoGeral)
 
@@ -76,17 +96,38 @@ def EscolherHorarioClinicoGeral(mensagem):
     /4 - 9:30
     /5 - 10:00
     """
+    chat_id = mensagem.chat.id
+    user = user_dict[chat_id]
+    dia = mensagem.text  
+    user.data = dia    
     bot.send_message(mensagem.chat.id, texto)
-    bot.register_next_step_handler(mensagem, EscolherClinicoGeral)
+    bot.register_next_step_handler(mensagem, confirmarClinico)
 
-def confirmarClinico(mensagem): 
-    texto = """
+def confirmarClinico(mensagem):  
+    chat_id = mensagem.chat.id
+    user = user_dict[chat_id]
+    hora = mensagem.text  
+    user.hora = hora       
+    texto = f"""
     Confirma a consulta?
+    nome{user.name}
+    medico{user.medico}
+    data{user.data}
+    hora{user.hora}
     /1 - Sim
     /2 - Não
     """
+    
     bot.send_message(mensagem.chat.id, texto)
-    bot.register_next_step_handler(mensagem, EscolherClinicoGeral)
+    bot.register_next_step_handler(mensagem, sucessoClinico)
+
+def sucessoClinico(mensagem):     
+    texto = """
+    consulta marcada com sucesso!
+    """
+    bot.send_message(mensagem.chat.id, texto)    
+
+
 
 
 
@@ -103,5 +144,16 @@ def responder(mensagem):
      /CancelarConsulta Mandar um abraço pro Lira
 Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
     bot.reply_to(mensagem, texto)
+
+
+# Enable saving next step handlers to file "./.handlers-saves/step.save".
+# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
+# saving will hapen after delay 2 seconds.
+#bot.enable_save_next_step_handlers(delay=2)
+
+# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+# WARNING It will work only if enable_save_next_step_handlers was called!
+#bot.load_next_step_handlers()
+
 
 bot.polling()
